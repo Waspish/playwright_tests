b5b0c419df8e1dbe6f2ef9e9247d1abeb6e55cde
waspish <ostapenko.andreik@gmail.com>
2025-08-08 07:57:34 -0700

    Conftest mistake config

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 
+import allure
 import pytest
 from playwright.sync_api import Page
 
@@ -8,6 +9,8 @@ from playwright.sync_api import Page
 def page(page: Page, request):
     yield page
     if request.node.rep_call.failed:
-        timestamp = datetime.now().strftime('%Y.%m.%d.%H.%M.%S')
-        screenshot_path = os.path.join('screenshots', timestamp)
-        page.screenshot(path=f'test_failed_{screenshot_path}.png')
+        # timestamp = datetime.now().strftime('%Y.%m.%d.%H.%M.%S')
+        # screenshot_path = os.path.join('screenshots', timestamp)
+        # page.screenshot(path=f'test_failed_{screenshot_path}.png')
+        screenshot = page.screenshot(full_page=True)
+        allure.attach(screenshot, name="Mistake_test_chrome", attachment_type=allure.attachment_type.PNG)
